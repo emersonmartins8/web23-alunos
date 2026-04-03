f44b012b40a67e041fcac39fec4bb7f4f55108bd
emersonmartins8 <emersonmartins8@gmail.com>
2026-04-03 16:19:14 -0300

    feat: update blockchain implementation with Block class enhancements and new Blockchain class
    - Added properties to Block class: timestamp, previousHash, and data.
    - Implemented constructor for Block class to initialize properties and calculate hash.
    - Introduced getHash method in Block class to compute the block's hash using sha256.
    - Created a new Blockchain class to manage a chain of blocks, starting with a genesis block.
    - Updated package.json to include necessary dependencies for crypto-js and testing.
    - Modified tsconfig.json for module resolution and target settings.

--- protochain/src/block.ts
+++ protochain/src/block.ts
@@ -1,6 +1,37 @@
+import sha256 from "crypto-js/sha256.js";
+
 export default class Block {
     index: number = 1;
+    timestamp: number = Date.now();
     hash: string = "";
+    previousHash: string = "";
+    data : string = "" ;
+
+
+/**
+ * 
+ * @param index The block index in blockchain
+ * @param timestamp The timestamp data in blockchain
+ * @param hash The block hash
+ * @param previousHash The previous block hash
+ * @param data The block data
+ */
+
+    constructor(index: number, timestamp: number, hash: string, previousHash: string, data: string) {
+        this.index = index;
+        this.timestamp = timestamp;
+        this.previousHash = previousHash;
+        this.data = data;
+        this.hash = this.getHash();
+    
+    }
+
+    getHash(): string {
+        return sha256(this.index + this.data + this.timestamp + this.previousHash).toString();
+    }
+
+
+
 
     isValid(): boolean {
         if (this.index < 0) return false;    
--- protochain/src/blockchain.ts
+++ protochain/src/blockchain.ts
@@ -1,9 +1,13 @@
 import Block from './block';
 
-const block1 = new Block();
-block1.hash = "abc123";
-block1.index = 2;
+/**
+ * A simple implementation of a blockchain in TypeScript. This class manages a chain of blocks, allowing you to add new blocks and validate the integrity of the chain. Each block contains an index, timestamp, hash, previous hash, and data. The blockchain starts with a genesis block, and new blocks are added by referencing the hash of the latest block in the chain.
+ */
 
-block1.isValid();
-
-console.log(block1.isValid());
+export default class Blockchain {
+    blocks: Block[];
+    
+    constructor() {
+        this.blocks = [new Block(0,"","Genesis Block", "", "")];
+    }
+}
